1865c3495ed1a13085603582a6216a057f739d48
Ricardo Matias <ricardo.matias0@gmail.com>
2019-10-01 14:42:12 +0200

    add cli entry point

---
+++ cli.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node --no-warnings
+
+const { Readable } = require('stream');
+const meow = require('meow');
+const chalk = require('chalk');
+const { parseLyrics } = require('./');
+
+const cli = meow(chalk`
+  {yellow.bold Usage}
+      $ lyrics [artist] [song] [-c]
+  {yellow.bold Options}
+      -c, --clean  Just lyrics without title
+  {yellow.bold Examples}
+      $ {green.bold lyrics} radiohead reckoner
+      {grey > radiohead - reckoner [In Rainbows]}
+      {grey > Reckoner,}
+      {grey > You can't take it with you, ..}
+`, {
+    flags: {
+        clean: {
+            type: 'boolean',
+            alias: 'c',
+            default: false,
+        },
+    }
+});
+
+const [artist, song] = cli.input;
+
+if (!artist || !song) {
+    cli.showHelp();
+}
+
+const main = async () => {
+    const res = await parseLyrics(artist, song);
+    let lyrics = res.lyrics.split('\n').map((line) => {
+        if (line[0] == ',') {
+            line = line.substr(1, line.length);
+        }
+        return line;
+    }).join('\n');
+
+    let title = `${artist} - ${song} [${res.album}]\n\n`;
+
+    if (cli.flags.clean) {
+        title = '';
+    }
+
+    const body = `${title}${lyrics}`;
+
+    const outStream = new Readable({ read() {} });
+
+    outStream.push(body);
+
+    outStream.pipe(process.stdout);
+}
+
+main();
